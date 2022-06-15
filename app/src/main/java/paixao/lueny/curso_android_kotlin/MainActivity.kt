596704dc1a74fc70d471acb95206687c09bb4b8d
Lueny Dantas <luenypaixao@gmail.com>
2022-06-15 00:15:44 -0300

    feat: setup textviews

--- app/src/main/java/paixao/lueny/curso_android_kotlin/MainActivity.kt
+++ app/src/main/java/paixao/lueny/curso_android_kotlin/MainActivity.kt
@@ -2,10 +2,17 @@ package paixao.lueny.curso_android_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val nome = findViewById<TextView>(R.id.nome)
+        nome.text = "Cesta de frutas"
+        val descricao = findViewById<TextView>(R.id.descricao)
+        descricao.text = "Laranja, manga e maçã"
+        val valor = findViewById<TextView>(R.id.valor)
+        valor.text = "19.99"
     }
 }
