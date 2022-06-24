251fe93d97c121cf1b3dff5c0c1d32a21a079799
Lueny Dantas <luenypaixao@gmail.com>
2022-06-24 00:44:09 -0300

    feat: setup Activity

--- app/src/main/java/paixao/lueny/curso_android_kotlin/Produto/Product.kt
+++ app/src/main/java/paixao/lueny/curso_android_kotlin/Produto/Product.kt
@@ -2,7 +2,7 @@ package paixao.lueny.curso_android_kotlin.Produto
 
 import java.math.BigDecimal
 
-class Product(
+ data class Product(
     val name: String,
     val description: String,
     val value: BigDecimal
