fe6f75ac326e0b757f62935d3d19402ce607c089
Lueny Dantas <luenypaixao@gmail.com>
2022-12-15 18:12:17 -0300

    fix: add suspended prefix into dao interface

--- app/src/main/java/paixao/lueny/curso_android_kotlin/database/dao/ProductDao.kt
+++ app/src/main/java/paixao/lueny/curso_android_kotlin/database/dao/ProductDao.kt
@@ -1,39 +1,40 @@
 package paixao.lueny.curso_android_kotlin.database.dao
 
 import androidx.room.*
-import paixao.lueny.curso_android_kotlin.Produto.Product
+import kotlinx.coroutines.flow.Flow
+import paixao.lueny.curso_android_kotlin.model.Product
 
 
 @Dao
 interface ProductDao {
 
     @Query("SELECT * FROM Product")
-    fun searchAll(): List<Product>
+    fun searchAll(): Flow<List<Product>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun save(vararg product: Product)
+    suspend fun save(vararg product: Product)
 
     @Delete
-    fun remove(product: Product)
+    suspend fun remove(product: Product)
 
-    @Query("SELECT * FROM Product WHERE id = :id")
-    fun searchById(id: Long): Product?
+    @Query("SELECT * FROM Product WHERE id = :productId")
+    suspend fun searchById(productId: Long): Product?
 
     @Query("SELECT * FROM Product ORDER BY name ASC")
-    fun searchAllSortbyNameAsc():List<Product>
+    fun searchAllSortbyNameAsc():Flow<List<Product>>
 
     @Query("SELECT * FROM Product ORDER BY name DESC")
-    fun searchAllSortByNameDesc():List<Product>
+    fun searchAllSortByNameDesc(): Flow<List<Product>>
 
     @Query("SELECT * FROM Product ORDER BY description ASC")
-    fun searchAllSortByDescriptionAsc(): List<Product>
+    fun searchAllSortByDescriptionAsc():Flow<List<Product>>
 
     @Query("SELECT * FROM Product ORDER BY description DESC")
-    fun searchAllSortByDescriptionDesc(): List<Product>
+    fun searchAllSortByDescriptionDesc(): Flow<List<Product>>
 
     @Query("SELECT * FROM Product ORDER BY value ASC")
-    fun searchAllSortByValueAsc(): List<Product>
+    fun searchAllSortByValueAsc():Flow<List<Product>>
 
     @Query("SELECT * FROM Product ORDER BY value DESC")
-    fun searchAllSortByValueDesc(): List<Product>
+    fun searchAllSortByValueDesc():Flow<List<Product>>
 }
