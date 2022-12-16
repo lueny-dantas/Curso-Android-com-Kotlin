e7d06ca6d9268bda2cebedc108e2592932e28a76
Lueny Dantas <luenypaixao@gmail.com>
2022-12-15 23:37:51 -0300

    feat: implementation DataStore preferences

---
+++ app/src/main/java/paixao/lueny/curso_android_kotlin/preferences/userPreferences.kt
@@ -0,0 +1,11 @@
+package paixao.lueny.curso_android_kotlin.preferences
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "user_session")
+
+val userLoggedPreferences = stringPreferencesKey("Usuário Logado")
